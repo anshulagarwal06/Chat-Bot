cbb62ad5d0a8a02b2cae29c382120a98f73d39bc
Anshul Agarwal <anshul1.agarwal@peppertap.com>
2017-02-07 16:33:37 +0530

    add postgres

--- projectD/settings.py
+++ projectD/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'projectD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projectd_database',
+        'USER': 'anshul',
+        'PASSWORD': 'anshul',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
