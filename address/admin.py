029d5d55b1015007d95934b96ceaea815a616394
Anshul Agarwal <anshul1.agarwal@peppertap.com>
2017-02-24 14:41:08 +0530

    Added customeraddress and storeaddress in adminsite

--- address/admin.py
+++ address/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Addresses
+from .models import Addresses, CustomerAddress
 
 admin.site.register(Addresses)
+admin.site.register(CustomerAddress)
--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Store
+from .models import Store, StoreAddress
 
 admin.site.register(Store)
+admin.site.register(StoreAddress)
