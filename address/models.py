691b2eb920b7ccff0a04da18cdaa53c1c90d61f4
Anshul Agarwal <anshul1.agarwal@peppertap.com>
2017-02-26 09:59:31 +0530

    Change address.title field limit to 100

--- address/models.py
+++ address/models.py
@@ -10,7 +10,7 @@ from accounts.models import Customers
 class Addresses(models.Model):
     latitude = models.DecimalField(decimal_places=6, max_digits=10)
     longitude = models.DecimalField(decimal_places=6, max_digits=10)
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
     address = models.CharField(max_length=200, blank=True);
     create_at = models.DateTimeField(auto_now_add=True);
 
