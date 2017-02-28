c5e3efef14c252f3b9bcac868d12a742365221f9
Anshul Agarwal <anshul1.agarwal@peppertap.com>
2017-02-28 11:43:02 +0530

    mda

--- app/views.py
+++ app/views.py
@@ -80,7 +80,7 @@ def sentTextMessage(recipientId, messageText, quick_replies=None, attachment=Non
     if quick_replies:
         message['quick_replies'] = json.dumps(quick_replies)
     if attachment:
-        message['attachment'] = json.dumps(attachment);
+        message['attachment'] = attachment;
 
     data['message'] = message
     data['recipient'] = recipient;
