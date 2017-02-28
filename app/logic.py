ac53cbb4db7ea3e6e346fb44f0db4b2d51c05f95
Anshul Agarwal <anshul1.agarwal@peppertap.com>
2017-02-28 14:33:16 +0530

    fix error

--- app/logic.py
+++ app/logic.py
@@ -6,7 +6,7 @@ from const import *
 
 def handle_category_reply(sender_id, category_id):
     # get customers
-    customer = account_models.models.fetch_customers_details(sender_id)
+    customer = account_models.fetch_customers_details(sender_id)
 
     store = store_models.get_customers_store(customer);
     store_product = store_models.StoreProducts.objects.filter(store=store, product__Category__id=category_id);
